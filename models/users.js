a63d0d866bd948dc4446af088583b373ff975898
Jatin Ramchandani <jatinramchandani307@gmail.com>
2022-06-02 02:12:39 +0530

    heroku deploy

---
+++ models/users.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose');
+
+var SchemaTypes = mongoose.Schema.Types;
+const userSchema=new mongoose.Schema({
+
+    first_name:{
+        type: String,
+        required: true
+    },
+
+    last_name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        require: true
+        
+    },
+    password:{
+        type: String,
+        reqruired: true
+    },
+})
+
+module.exports=mongoose.model('users',userSchema)
---
+++ routes/users.js
@@ -0,0 +1,161 @@
+const e = require('express');
+const express = require('express');
+const path = require("path");
+const router = express.Router();
+const user = require('../models/users');
+const jwt= require('jsonwebtoken');
+const { verify } = require('crypto');
+
+
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+router.post('/signup', async (req, res) => {
+
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+    let email = req.body.email;
+    let password = req.body.password;
+
+    try {
+        if(!email.match(mailformat)){
+            res.status(400).send("Invalid Email format");
+        }
+        const finduser = await user.findOne({ email: email });
+        if (finduser!=null) {
+            console.log(finduser);
+            res.status(400).send("User already Exists")
+        }
+        else {
+            const nuser = new user({
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                password: password,
+            });
+            const newuser = await nuser.save()
+            console.log(newuser);
+            res.status(201).send({ "result": "user added" });
+        }
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+})
+
+
+
+router.post('/login', async (req, res) => {
+
+    let email = req.body.email;
+    let password = req.body.password;
+
+    console.log({ "email": email, "pass": password });
+
+    try {
+        if(!email.match(mailformat)){
+            res.status(400).send("Invalid Email format");
+        }
+        const thisuser = await user.findOne({ email: email });
+        if (thisuser.password === password) {
+            console.log(thisuser);
+            res.send(thisuser);
+        } else {
+            res.status(400).send("password is not matching");
+        }
+    }
+    catch (err) {
+        res.status(400).json("Invalid Email");
+    }
+
+
+})
+
+
+router.post('/forget-password', async (req, res) => {
+
+    let email = req.body.email;
+
+    console.log({ "email": email});
+
+    try {
+        if(!email.match(mailformat)){
+            res.status(400).send("Invalid Email format");
+        }
+        const thisuser = await user.findOne({ email: email});
+        if(thisuser==null){
+            res.send("User doesn't exist");
+        }
+        
+        const secret = process.env.JWT_SECRET + thisuser.password;
+        const payload = {
+            email: thisuser.email,
+            id: thisuser._id
+        }
+
+        const token = jwt.sign(payload, secret, {expiresIn: '15m'});
+        const link = `http://localhost:8000/api/users/reset-password/${thisuser._id}/${token}`;
+        console.log(link);
+        res.status(200).send("Password reset link has been sent to your email...");
+
+
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+
+
+})
+
+
+router.get('/reset-password/:id/:token/', async (req, res) => {
+
+    let id = req.params.id;
+    let token = req.params.token;
+
+    try {
+        const thisuser = await user.findOne({ _id: id});
+        if(thisuser==null){
+            res.send("Invalid Id");
+        }
+        const secret = process.env.JWT_SECRET + thisuser.password;
+        const payload = jwt.verify(token, secret);
+        res.status(200).send(payload);
+        // at response we will go to the reset password page
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+
+})
+
+
+router.post('/reset-password/:id/:token/', async (req, res) => {
+
+    let id = req.params.id;
+    let token = req.params.token;
+
+    let password = req.body.pass_before;
+    let password2 = req.body.pass_after;
+
+    try {
+        const thisuser = await user.findOne({ _id: id});
+        if(thisuser==null){
+            res.send("Invalid Id");
+        }
+        const secret = process.env.JWT_SECRET + password;
+        const payload = jwt.verify(token, secret);
+
+        const changeuser = await user.updateOne({_id: id},{$set:{password: password2}})
+        res.status(200).send("password changed");
+
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+
+})
+
+
+
+module.exports = router
